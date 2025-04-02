621ebb3f5ff2397168003992f9fd92497e448f37
Anup Pradhan <anuppradhan929@gmail.com>
2025-04-02 12:27:05 +0530

    Adding proper front end and back end

---
+++ Backend/index.js
@@ -0,0 +1,32 @@
+import express from 'express'
+
+const app = express()
+
+const port = process.env.PORT || 8080
+
+app.get('/', (req, res) => {
+    res.send(`Hello world `)
+});
+
+app.get('/jokes', (req, res) => {
+    const jokes = [
+        {
+            id:1,
+            title: "A joke",
+            content: "This is a joke",
+        },
+        {
+            id:2,
+            title: "Another joke",
+            content: "This is another joke",
+        }
+    ]
+    res.send(jokes)
+});
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+// http://localhost:8080/
